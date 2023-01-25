836257368c9e2c974f78c2bf4b9162ec3b32d97f
Eddie Gutman <edigutman@gmail.com>
2023-01-25 21:20:00 +0200

    Fix loginController
    Fixed a bug in loginController to work correctly.

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -7,18 +7,19 @@ const usersBLL = require("../BLL/usersBLL");
 const router = express.Router();
 
 // Login
-router.post("/login", async (request, response) => {
+router.post("/", async (request, response) => {
     try {
         // get the user login details
         const { username, password } = request.body;
 
         // search if the user exists in the database
-        const user = await usersBLL.getUser(username, password);
+        const users = await usersBLL.getUser(username, password);
+        const user = users[0];
         if (user) {
             // create a token for the user
             const userID = user._id;
             const RSA_PRIVATE_KEY = "BoostApp";
-            const token = jwt.sign({ userID: userID }, RSA_PRIVATE_KEY);
+            const token = jwt.sign({ id: userID }, RSA_PRIVATE_KEY);
 
             // return the token to the user with a status
             response.status(200).json({ token });
