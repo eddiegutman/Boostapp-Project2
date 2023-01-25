fb75068ae45e0f7bd44cb7c1b961a31e903d7ef0
Eddie Gutman <edigutman@gmail.com>
2023-01-25 20:26:16 +0200

    Add config
    Added a config to connect the database

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 // import libraries 
 const express = require("express");
+const connectDB = require("./Config/database");
 
 // import controllers
 const loginController = require("./Controllers/loginController");
 
 // create the server
 const app = express();
+const port = 8000;
+
+// connect to the database
+connectDB();
 
 // add server settings
 app.use(express.json());
@@ -14,6 +19,6 @@ app.use(express.json());
 app.use("/login", loginController);
 
 // run the server
-app.listen(8000, ()=> {
+app.listen(port, ()=> {
     console.log("Server is running");
 })
