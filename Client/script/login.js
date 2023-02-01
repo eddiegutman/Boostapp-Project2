609c555aeca3d458b9316878f1c9897aba7d56e9
Eddie Gutman <edigutman@gmail.com>
2023-02-02 00:01:47 +0200

    Add the login page

---
+++ Client/script/login.js
@@ -0,0 +1,18 @@
+const login = async () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const fetchParams = {
+        method: "POST",
+        headers: {"Content-Type": "Application/json"},
+        body: JSON.stringify({username: username, password: password})
+    };
+
+    const response = await fetch("http:/localhost:8000/login", fetchParams);
+
+    if (response.status = 200) {
+        const token = await response.json();
+        sessionStorage["x-access-token"] = token;
+        window.location.href = "../html/homepage.html";
+    }
+}
