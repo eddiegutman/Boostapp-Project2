c4e5cef8d4f676f0de71645f1c9b3fd6dd088163
Eddie Gutman <edigutman@gmail.com>
2023-02-02 02:19:36 +0200

    Add navigation bar to homepage
    Added a navigation bar which shows the user's name and a logout button.

--- Client/script/homepage.js
+++ Client/script/homepage.js
@@ -9,5 +9,26 @@ const load = () => {
     }
 }
 
+const navBar = () => {
+    const nav = document.getElementById("nav");
+    const spanName = document.createElement("span");
+    const spanButton = document.createElement("span");
+    const logoutButton = document.createElement("button");
+
+    const name = sessionStorage.getItem("name");
+    spanName.textContent = `Hello ${name}`;
+
+    logoutButton.textContent = "Logout";
+    logoutButton.addEventListener("click", logout);
+    spanButton.append(logoutButton);
+
+    nav.append(spanName, spanButton);
+}
+
+const logout = () => {
+    sessionStorage.clear();
+    window.location.href = "../html/login.html"
+}
+
 const body = document.getElementById("body");
-body.addEventListener("load", load());
+body.addEventListener("load", load(), navBar());
