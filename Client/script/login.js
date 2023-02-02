1c0e354069ebc92c84a3570cb3488080af9455ca
Eddie Gutman <edigutman@gmail.com>
2023-02-02 10:46:47 +0200

    Add Enter key press event to trigger the login

--- Client/script/login.js
+++ Client/script/login.js
@@ -4,14 +4,14 @@ const login = async () => {
 
     const fetchParams = {
         method: "POST",
-        headers: {"Content-Type": "Application/json"},
-        body: JSON.stringify({username: username, password: password})
+        headers: { "Content-Type": "Application/json" },
+        body: JSON.stringify({ username: username, password: password })
     };
 
     const response = await fetch("http:/localhost:8000/login", fetchParams);
 
     if (response.status == 200) {
-        const {token, name} = await response.json();
+        const { token, name } = await response.json();
         sessionStorage["x-access-token"] = token;
         sessionStorage["name"] = name;
         window.location.href = "../html/homepage.html";
@@ -22,3 +22,14 @@ const login = async () => {
 
 const loginButton = document.getElementById("loginButton");
 loginButton.addEventListener("click", login);
+
+// Execute a function when the user presses a key on the keyboard
+document.addEventListener("keypress", (event) => {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key == "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        loginButton.click();
+    }
+});
