681ab4257110fcac097e56d1054c32142c8ef2e4
Eddie Gutman <edigutman@gmail.com>
2023-02-03 22:35:46 +0200

    Add new department section
    Add:
    - addDepartment.html
    - addDepartment.js

---
+++ Client/script/addDepartment.js
@@ -0,0 +1,72 @@
+// registered user detection function
+const load = async () => {
+    // get the token from session storage
+    const token = sessionStorage.getItem("x-access-token");
+    // check if token exists and show content accordingly
+    if (token) {
+        document.getElementById("noLogin").remove();
+        loadData();
+    } else {
+        document.getElementById("mainDiv").remove();
+    }
+}
+
+// add department page loading function
+const loadData = async () => {
+    // create the request
+     const fetchParams = {
+        method: "GET",
+        headers: {
+            "Content-Type": "Application/json",
+            "x-access-token": sessionStorage.getItem("x-access-token")
+        }
+    };
+
+    // request all employees and save response
+    const response = await fetch(`http:/localhost:8000/employees`, fetchParams);
+    const employees = await response.json();
+
+    // get element
+    const selectDepManager = document.getElementById("depManager");
+    
+    // insert all the employees into the select input
+    for (let employee of employees) {
+        const option = document.createElement("option");
+        option.textContent = `${employee.firstName} ${employee.lastName}`;
+        option.value = employee._id;
+        selectDepManager.append(option);
+    }
+
+}
+
+// the add button function
+const add = async () => {
+    // create the request
+    const fetchParams = {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json",
+            "x-access-token": sessionStorage.getItem("x-access-token")
+        },
+        body: JSON.stringify({
+            name: document.getElementById("depName").value,
+            manager: document.getElementById("depManager").value
+        })
+    };
+
+    // request add department, alert response and redirect page
+    const response = await fetch(`http:/localhost:8000/departments`, fetchParams);
+    const status = await response.json();
+    alert(status);
+    window.location.href = "../html/departments.html";
+}
+
+// assign onclick event to the add department button
+const buttonAdd = document.getElementById("buttonAdd");
+buttonAdd.addEventListener("click", add);
+
+// add the load and nav bar functions to the body element as an onload event
+document.body.onload = async function() {
+    await load();
+    await navBar();
+};
