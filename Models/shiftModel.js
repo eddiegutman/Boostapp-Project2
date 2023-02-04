164678538767b215f2080641264287cccfef5d44
Eddie Gutman <edigutman@gmail.com>
2023-02-04 13:00:53 +0200

    Update shift model time format to string from int

--- Models/shiftModel.js
+++ Models/shiftModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const shiftSchema = new mongoose.Schema({
     date: String,
-    startTime: Number,
-    endTime: Number
+    startTime: String,
+    endTime: String
 });
 
 const Shift = mongoose.model("shifts", shiftSchema);
