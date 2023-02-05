724fd57821909e09b203318773bf2be8e9435298
Eddie Gutman <edigutman@gmail.com>
2023-02-05 03:46:03 +0200

    Update the limit function
    Limit function now supports multiple users from the same browser

--- Client/script/login.js
+++ Client/script/login.js
@@ -16,16 +16,17 @@ const login = async () => {
 
     // if response is ok save the user's token and name and move to the homepage
     if (response.status == 200) {
-        const { token, name, limit } = await response.json();
+        const { token, name, id, limit } = await response.json();
         sessionStorage.setItem("x-access-token", token);
         sessionStorage.setItem("name", name);
+        sessionStorage.setItem("userID", id);
 
         // use to reset actions remaining actions for testing
         //localStorage.clear();
         //localStorage.setItem("remainingActions", 20);
 
         // get the last login date
-        const lastLogin = localStorage.getItem("lastLogin");
+        const lastLogin = localStorage.getItem(`lastLogin_${id}`);
         // if user ever logged in
         if (lastLogin) {
             // get current date
@@ -33,26 +34,26 @@ const login = async () => {
             // check if user already logged in today
             if (lastLogin == currentDate) {
                 // check if user has remaining actions for today and redirect accordingly
-                const remainingActions = +localStorage.getItem("remainingActions");
+                const remainingActions = +localStorage.getItem(`remainingActions_${id}`);
                 if (remainingActions == 0) {
                     alert("Action limit reached for today");
                     window.location.href = "../html/limit.html";
                 } else {
                     window.location.href = "../html/homepage.html";
                 }
-            // if user havn't loggeg in today
+            // if user hasn't logged in today
             } else {
                 // reset his actions limit and update last logged in date
-                localStorage.setItem("remainingActions", +limit);
-                localStorage.setItem("lastLogin", currentDate);
+                localStorage.setItem(`remainingActions_${id}`, +limit);
+                localStorage.setItem(`lastLogin_${id}`, currentDate);
                 window.location.href = "../html/homepage.html";
             }
         // if user first time log in
         } else {
             // update his actions limit and last logged in date
             const currentDate = new Date().toJSON().slice(0, 10);
-            localStorage.setItem("lastLogin", currentDate);
-            localStorage.setItem("remainingActions", +limit);
+            localStorage.setItem(`lastLogin_${id}`, currentDate);
+            localStorage.setItem(`remainingActions_${id}`, +limit);
             window.location.href = "../html/homepage.html";
         }
     } else {
